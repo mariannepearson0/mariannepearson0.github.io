ef1bb731af74b04cb20aa097a189248ca5a4dff1
mariannepearson0 <32272884+mariannepearson0@users.noreply.github.com>
2017-09-30 15:43:46 +0100

    Add files via upload

--- sidebar/sidebar/ChildPages/fizzbuzz.js
+++ sidebar/sidebar/ChildPages/fizzbuzz.js
@@ -1,31 +1,43 @@
-var no = 50
-var three = 'fizz'
-var five = 'buzz'
 
-function fizzbuzz (no, three, five) {
+function fizzbuzz () {
+  let no = document.getElementById("range").value
+  let three = document.getElementById("threes").value
+  let five = document.getElementById("fives").value
   for (let i = 1; i <= no; i++) {
     if (i % 3 === 0) {
       let p1 = document.createElement('p')
       p1.setAttribute('class', 'pfizz')
-      p1.innerHTML = three
+      if(three === ''){
+        three = 'fizz'
+        p1.innerHTML = 'fizz'
+      }
+      else{
+        p1.innerHTML = three
+      }
       document.getElementsByTagName('body')[0].appendChild(p1)
-    }
+    } 
     if (i % 5 === 0) {
       let p1 = document.createElement('p')
       p1.setAttribute('class', 'pbuzz')
+      if(five === ''){
+        five = 'buzz'
+        p1.innerHTML = 'buzz'
+      }
+      else{
       p1.innerHTML = five
+      }
       document.getElementsByTagName('body')[0].appendChild(p1)
-    }
+    } 
     if (i % 3 === 0 && i % 5 === 0) {
       let p1 = document.createElement('p')
       p1.setAttribute('class', 'pfizzbuzz')
-      p1.innerHTML = 'FizzBuzz'
+      p1.innerHTML = three + five
       document.getElementsByTagName('body')[0].appendChild(p1)
     } else {
-      //document.getElementById('fizz').innerHTML += i + ' '
       let p1 = document.createElement('p')
       p1.innerHTML = i
       document.getElementsByTagName('body')[0].appendChild(p1)
     }
   }
 }
+
