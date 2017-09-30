f6f51aaf2a3612e5db4cba6627936365de5c6e84
mariannepearson0 <32272884+mariannepearson0@users.noreply.github.com>
2017-09-30 15:08:22 +0100

    Add files via upload

--- sidebar/sidebar/ChildPages/triples.js
+++ sidebar/sidebar/ChildPages/triples.js
@@ -1,13 +1,12 @@
 function triple() {
+  document.getElementsByTagName("tripp").innerHTML = '';
   let tripstr = document.getElementById('tripst').value
   let counter = 0
   for (let i = 0; i < tripstr.length; i++) {
     if ((tripstr.charAt(i) === tripstr.charAt(i + 1)) && (tripstr.charAt(i + 1) === tripstr.charAt(i + 2))) {
-      counter = counter + 1
+      counter ++
     }
   }
-  let myp = document.createElement("p");
-  myp.textContent = `The number of triples is ${counter}`;
-  document.getElementsByTagName("body")[0].appendChild(myp);
-  //document.getElementById('tripstring').innerHTML = `The number of triples is ${counter}`
+  document.getElementById("tripp").innerHTML = `For the string "${tripstr}" The number of triples is: ${counter}`;
+  document.getElementById('tripst').value = ''
 }
